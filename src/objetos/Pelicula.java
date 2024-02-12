7b14811e71036d962a9d0639d65c4ad97d66db4e
JUAN CARLOS CANDELAS TORRES <claudiacandelas2005@gmail.com>
2024-02-12 19:38:07 +0100

    (doc): Documentación del constructor de la clase Pelicula

--- src/objetos/Pelicula.java
+++ src/objetos/Pelicula.java
@@ -5,6 +5,7 @@ public class Pelicula {
     private String titulo;
     private int duracion;
 
+    // Este constructor nos permite acceder al título y a la duración de las películas
     public Pelicula(String titulo, int duracion) {
         this.titulo = titulo;
         this.duracion = duracion;
